29973ecd0832c07214e4c01544fecf1df69fe21b
Jimmmmmm <chhimpojim226@gmail.com>
2026-06-01 22:53:25 +0700

    updated

--- src/app/select-location/page.tsx
+++ src/app/select-location/page.tsx
@@ -3,7 +3,7 @@ import LocationSelector from "../components/LocationSelector";
 export default function page() {
   return (
     <main className="mx-auto max-w-xl p-6">
-      <h1 className="mb-6 text-2xl font-bold">Test Location Dropdown</h1>
+      <h1 className="mb-6 text-4xl font-bold underline text-center">Location Selector</h1>
       <LocationSelector />
     </main>
   );
